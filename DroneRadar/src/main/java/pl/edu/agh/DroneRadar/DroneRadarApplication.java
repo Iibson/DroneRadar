c09ab5ae230a985fec80be423e472a79a4196cb8
Krzysztof Wójtowicz <401284@student.agh.edu.pl>
2022-05-07 23:32:09 +0200

    init backend

---
+++ DroneRadar/src/main/java/pl/edu/agh/DroneRadar/DroneRadarApplication.java
@@ -0,0 +1,13 @@
+package pl.edu.agh.DroneRadar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DroneRadarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DroneRadarApplication.class, args);
+	}
+
+}
